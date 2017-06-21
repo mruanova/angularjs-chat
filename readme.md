28f8e4be6b055fdcba20806f801f8bcb28f847dd
Mauricio Ruanova-Hurtado <mruanova@yahoo.com>
2017-06-21 14:56:14 -0500

    coding dojo assignments new repo discuss

---
+++ readme.md
@@ -0,0 +1,27 @@
+Mauricio Ruanova-Hurtado. MEAN (MongoDB ExpressJS AngularJS NodeJS) discussion board.
+
+Assignment: Discussion Board
+This is our final MEAN project before the belt exam. 
+Once you create the basic functionality described in the wire-frame below, try adding new features. 
+Use git and GitHub to collaborate with your peers!
+
+Optional Pieces:
+
+1) Build a full login and registration.
+2) Use passport to further improve your login and registration! http://passportjs.org/ 
+3) Use JWT (JavaScript Web Tokens) to maintain your logged in user info! https://jwt.io/
+
+Required Features:
+Display of all Topics posted by the users and the real-time count of posts/answers for that specific topic.
+Able to add a new topic and its description based on pre-defined/entered categories (saved in the database). 
+Users’ ability to put answers/post and comments to each topic.
+Ability to up-vote and down-vote an answer. 
+The count should update real-time.
+User profile page that displays how many topics, answers and comments the user has posted.
+Login/Logout feature.
+GitHub is a social network for programmers. 
+You can look at what other programmers are working on, check their code and make suggestions or changes. 
+You may share projects you are working on and collaborate with other programmers for updates or additional features.
+Upload your work for this exercise on GitHub and ask some of your cohort mates or other programmers to peruse your code and suggest changes/updates. 
+Do the same thing for your colleagues’ work.
+Put your GitHub repository link on a text file and upload the text file here.
---
+++ server.js
@@ -0,0 +1,171 @@
+var express = require('express');
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require("bcryptjs");
+var regex_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var regex_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./client"))); // index.html
+app.use(express.static(path.join(__dirname, "./bower_components")));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/discussdb', function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to Mongoose");
+    }
+});
+
+var usersSchema = new mongoose.Schema({
+	username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 8,
+        validate: {
+            validator: function (value) {
+                return regex_email.test(value);
+            },
+            message: "Email is invalid."
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 24,
+        validate: {
+            validator: function (value) {
+                return regex_password.test(value);
+            },
+            message: "Password failed validation, you must have at least 1 number, uppercase and special character"
+        }
+    }
+}, { timestamps: true });
+
+// 1 = Web Fundamentals, 2 = Python, 3 = MEAN, 4 = Ruby on rails, 5 = ASP.Net
+var categoriesSchema = new mongoose.Schema({
+	name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 20
+    }
+}, { timestamps: true });
+
+// html5, css3, git, heroku, aws; mysql, sqlite, apache, django, flask, jinja; mongodb, express, angularjs, nodejs; ruby, rails; c#, visual basic, sqlserver.
+var topicSchema = new mongoose.Schema({
+    _author: {
+        type: String,
+        ref: 'Users',
+        required: true
+    },
+	_category: {
+        type: String,
+        ref: 'Categories',
+        required: true
+    },
+	title: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+	description: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 140
+    }
+}, { timestamps: true });
+
+var postsSchema = new mongoose.Schema({
+    _author: {
+        type: String,
+        ref: 'Users',
+        required: true
+    },
+	postText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 1000,
+    },
+	likes:{ 
+		type: Number, 
+		default: 0,
+		required: true 
+	},
+	dislikes:{ 
+		type: Number, 
+		default: 0,
+		required: true 
+	}
+}, { timestamps: true });
+
+var commentsSchema = new mongoose.Schema({
+    _author: {
+        type: String,
+        ref: 'Users',
+        required: true
+    },
+    _post: {
+        type: String,
+        ref: 'Posts',
+        required: true
+    },
+    commentText: {
+        type: String,
+        minlength: 1,
+        maxlength: 140,
+        required: true,
+    }
+}, { timestamps: true })
+
+mongoose.model('Users', usersSchema);
+var UsersModel = mongoose.model('Users');
+
+/*
+mongoose.model(Categories, categoriesSchema);
+mongoose.model(Topics, topicsSchema);
+mongoose.model(Posts, postsSchema);
+mongoose.model(Comments, commentsSchema);
+ */
+
+var usersController = {
+    index: function (req, res) {
+        var promise = UsersModel.find({}).sort({ score: -1 }).exec();
+        promise.then(function (users) {
+            console.log("find all users: success!");
+            res.json({ users: users, errors: [] });
+        }).catch(function (err) {
+            console.log('find all users: error!', err);
+            res.json({ users: [], errors: err });
+        });
+    },
+}
+
+//app.get('/api', users.index);
+//app.post("/api/user", users.new_user);
+    app.post("/api/users", usersController.register);
+    app.post("/api/login", usersController.login);
+    app.post("/api/posts", usersController.addpost);
+    app.get("/api/posts", usersController.home);
+    app.post("/api/comments", usersController.addcomment);
+    app.get("/api/comments", usersController.home);
+
+app.listen(3000, function () {
+    console.log("listening on port 3000");
+});
