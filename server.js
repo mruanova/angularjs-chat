7aa7de3dd86b416e0ed8dd5a4907cafd3469c07f
mpbraun2 <mpbraun2@gmail.com>
2017-06-28 10:38:53 -0500

    update for NPM

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express  = require( 'express' ),
 
 app.use(express.static( path.join( root, './client' )));
 app.use(express.static( path.join( root, './bower_components')))
+app.use(express.static( path.join( root, './node_modules')))
 app.use(bp.json())
 
 require('./server/config/mongoose.js');
