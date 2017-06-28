a7959fc66f5e8cbc845d83d04bb124d9f2ca13ae
mpbraun2 <mpbraun2@gmail.com>
2017-06-28 17:09:40 -0500

    added username to posts

--- server/models/postsModel.js
+++ server/models/postsModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var postsSchema = new mongoose.Schema({
     _author: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Users',
         required: true
     },
